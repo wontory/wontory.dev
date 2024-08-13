a7dc86c43a7004cce23d6ffc60bbfc809b1b4507
조성원 <tjddnjsjo@kakao.com>
2024-08-14 06:09:40 +0900

    chore: update global CSS styles and Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,20 @@ export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		container: {
+			center: true,
+			padding: "0.75rem",
+		},
 		extend: {
+			colors: {
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
 			fontFamily: {
 				sans: ["Goorm Sans", ...defaultTheme.fontFamily.sans],
 				mono: ["Goorm Sans Code", ...defaultTheme.fontFamily.mono],
