e5345122ffa872e6d5bf01ef62f533599b3f68f1
조성원 <tjddnjsjo@kakao.com>
2024-08-14 03:49:08 +0900

    feat: add goorm-sans as global fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,16 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Goorm Sans", ...defaultTheme.fontFamily.sans],
+				mono: ["Goorm Sans Code", ...defaultTheme.fontFamily.mono],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
