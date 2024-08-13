25f8b77961f370d2cd9291efd34412fdd292b8c2
조성원 <tjddnjsjo@kakao.com>
2024-08-14 01:20:53 +0900

    chore: add astro-expressive-code astro plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.wontory.dev",
-  integrations: [tailwind()]
+	site: "https://www.wontory.dev",
+	integrations: [expressiveCode(), tailwind()],
 });
