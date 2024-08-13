d52baa6f73b5ad06b445350386a7f808fa35cd55
조성원 <tjddnjsjo@kakao.com>
2024-08-14 01:24:13 +0900

    chore: add mdx astro plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.wontory.dev",
-	integrations: [expressiveCode(), sitemap(), tailwind()],
+	integrations: [expressiveCode(), mdx(), sitemap(), tailwind()],
 });
