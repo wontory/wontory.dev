428b5b5c6ed5e42630505717a1de669813c11ef9
조성원 <tjddnjsjo@kakao.com>
2024-08-17 21:45:32 +0900

    fix: remove astro-icon from astro config

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.wontory.dev",
-  integrations: [expressiveCode(), icon(), mdx(), sitemap(), tailwind()],
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind()],
 });
