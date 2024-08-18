37da717d430e9ff204e028e31ccd3e1807569010
조성원 <tjddnjsjo@kakao.com>
2024-08-18 11:18:35 +0900

    feat: add date and reading time to blog and craft pages

---
+++ src/utils/date.ts
@@ -0,0 +1,6 @@
+export const formatDate = (date: Date) =>
+  date.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
---
+++ src/utils/reading-time.ts
@@ -0,0 +1,6 @@
+export const readingTime = (html: string) => {
+  const textOnly = html.replace(/<[^>]+>/g, "");
+  const wordCount = textOnly.split(/\s+/).length;
+  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
+  return `${readingTimeMinutes}분 소요`;
+};
