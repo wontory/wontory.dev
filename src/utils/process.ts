345a93355887edd858ec6517f301507dc902b50d
조성원 <tjddnjsjo@kakao.com>
2024-08-18 11:36:46 +0900

    refactor: edit code to use `process` utility function for filtering and sorting blog and craft posts

---
+++ src/utils/process.ts
@@ -0,0 +1,8 @@
+import type { CollectionEntry, ContentEntryMap } from "astro:content";
+
+export const process = <T extends keyof ContentEntryMap>(
+  entries: CollectionEntry<T>[],
+) =>
+  entries
+    .filter((entry) => !entry.data.draft)
+    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
