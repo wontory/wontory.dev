28df3111f3259ea80310a9abcd9682908b0e5211
조성원 <tjddnjsjo@kakao.com>
2024-08-18 13:07:46 +0900

    feat: add astro-icon integration and update dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.wontory.dev",
-  integrations: [expressiveCode(), mdx(), sitemap(), tailwind()],
+  integrations: [expressiveCode(), icon(), mdx(), sitemap(), tailwind()],
 });
