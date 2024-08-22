baadde2ffc83ffe77695bc81536baff65e4a44d8
조성원 <tjddnjsjo@kakao.com>
2024-08-22 20:00:08 +0900

    chore: passthrough image service

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -9,4 +9,7 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://www.wontory.dev",
   integrations: [expressiveCode(), icon(), mdx(), sitemap(), tailwind()],
+  image: {
+    service: passthroughImageService(),
+  }
 });
