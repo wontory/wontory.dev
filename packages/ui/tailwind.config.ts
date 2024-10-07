4caa992cf73ba8970664d65cc3935f2442b3bb4c
조성원 <tjddnjsjo@kakao.com>
2024-10-07 17:21:32 +0900

    feat(ui): add global styles

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -13,6 +13,25 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
       fontFamily: {
         sans: ['var(--font-pretendard)'],
       },
