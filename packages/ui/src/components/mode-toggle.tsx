192bca303674f66606ca463acbdb200a80ea76d7
조성원 <tjddnjsjo@kakao.com>
2024-10-08 18:50:30 +0900

    feat(ui): change icon size of mode-toggle component

--- packages/ui/src/components/mode-toggle.tsx
+++ packages/ui/src/components/mode-toggle.tsx
@@ -14,8 +14,8 @@ function ModeToggle() {
       size="icon"
       onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
-      <Sun className="dark:-rotate-90 size-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
-      <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="dark:-rotate-90 size-4 rotate-0 scale-100 transition-all dark:scale-0" />
+      <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
