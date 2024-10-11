22983785d0768a48d6122e154a02b28659c742c4
조성원 <tjddnjsjo@kakao.com>
2024-10-11 23:58:59 +0900

    feat(www): create playground page

---
+++ apps/www/app/playground/page.tsx
@@ -0,0 +1,13 @@
+export default function Playground() {
+  return (
+    <div className="container max-w-screen-lg space-y-8">
+      <div className="space-y-2 py-16 text-center">
+        <h1 className="font-semibold text-3xl">Playground</h1>
+        <p className="text-primary/60 text-sm">
+          Some boring things I've created for fun.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4" />
+    </div>
+  )
+}
