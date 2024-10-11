070835d4ca180c13ed40f231173dcdbceeaa6812
조성원 <tjddnjsjo@kakao.com>
2024-10-12 01:04:06 +0900

    fix(ui): remove path alias to resolve build error

--- packages/ui/src/components/mode-toggle.tsx
+++ packages/ui/src/components/mode-toggle.tsx
@@ -3,7 +3,7 @@
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
-import { Button } from '~/components/button'
+import { Button } from './button'
 
 function ModeToggle() {
   const { resolvedTheme, setTheme } = useTheme()
