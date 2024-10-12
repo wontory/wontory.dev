eceea1904cf0e5a9eb089e3db6ab72bddd66539c
조성원 <tjddnjsjo@kakao.com>
2024-10-13 07:46:53 +0900

    feat(www): add post metadata to post-card component

--- apps/www/src/components/post-card.tsx
+++ apps/www/src/components/post-card.tsx
@@ -1,4 +1,12 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@wontory/ui/card'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@wontory/ui/card'
+import { CalendarIcon, ClockIcon } from '@wontory/ui/lucide'
+import { format, ko } from '@wontory/util/date-fns'
 
 import type { Post } from '#site/content'
 
@@ -7,10 +15,24 @@ function PostCard({ post }: { post: Post }) {
     <Card className="shadow-none transition-colors hover:bg-primary/5">
       <CardHeader className="p-4">
         <CardTitle>{post.title}</CardTitle>
-        <CardDescription className="line-clamp-1 text-primary/60">
+        <CardDescription className="text-primary/60 md:line-clamp-1">
           {post.description}
         </CardDescription>
       </CardHeader>
+      <CardFooter className="p-4 pt-0">
+        <div className="flex justify-center gap-4 text-primary text-sm">
+          <span className="flex items-center gap-2">
+            <CalendarIcon className="size-3" />
+            {format(post.date, 'PPP', { locale: ko })}
+          </span>
+          <span className="flex items-center gap-2">
+            <ClockIcon className="size-3" />
+            {post.metadata.readingTime > 0
+              ? `${post.metadata.readingTime}분`
+              : '1분 이하'}
+          </span>
+        </div>
+      </CardFooter>
     </Card>
   )
 }
