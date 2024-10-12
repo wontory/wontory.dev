857cee11f8d474ea6faf802feb239ef6e1c35644
조성원 <tjddnjsjo@kakao.com>
2024-10-13 06:44:57 +0900

    refactor(www): rename lucide icon components

--- apps/www/src/components/mode-toggle.tsx
+++ apps/www/src/components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from '@wontory/ui/button'
-import { Moon, Sun } from '@wontory/ui/lucide'
+import { MoonIcon, SunIcon } from '@wontory/ui/lucide'
 import { useTheme } from '@wontory/ui/theme-provider'
 
 function ModeToggle() {
@@ -13,8 +13,8 @@ function ModeToggle() {
       size="icon"
       onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
-      <Sun className="dark:-rotate-90 size-4 rotate-0 scale-100 transition-all dark:scale-0" />
-      <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <SunIcon className="dark:-rotate-90 size-4 rotate-0 scale-100 transition-all dark:scale-0" />
+      <MoonIcon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
